5ca6e69706609ec3215fbe18b0bffe59b0c97afa
TwinProduction <twin@twinnation.org>
2020-07-04 19:39:23 -0400

    Add ready-to-go cache server in gocacheserver package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TwinProduction/gocache
 
 go 1.14
+
+require github.com/tidwall/redcon v1.3.2
---
+++ gocacheserver/gocacheserver.go
@@ -0,0 +1,114 @@
+package gocacheserver
+
+import (
+	"fmt"
+	"github.com/TwinProduction/gocache"
+	"github.com/tidwall/redcon"
+	"log"
+	"strings"
+	"time"
+)
+
+const (
+	// DefaultServerPort is the default port for the server
+	DefaultServerPort = 6379
+)
+
+// Server is a cache server using gocache as cache and RESP (Redis bindings) as server
+type Server struct {
+	Cache *gocache.Cache
+
+	// Port is the port that the server listens on
+	Port int
+
+	AutoSaveInterval time.Duration
+	AutoSaveFile     string
+}
+
+func NewServer(cache *gocache.Cache) *Server {
+	return &Server{
+		Cache: cache,
+		Port:  DefaultServerPort,
+	}
+}
+
+// WithAutoSave allows the configuration of the autosave feature interval at which
+// the cache will be automatically saved
+// Disabled if set to 0
+func (server *Server) WithAutoSave(interval time.Duration, file string) *Server {
+	server.AutoSaveInterval = interval
+	server.AutoSaveFile = file
+	return server
+}
+
+// WithPort sets the port of the server
+func (server *Server) WithPort(port int) *Server {
+	server.Port = port
+	return server
+}
+
+// Start starts the cache server, which includes the autosave
+func (server *Server) Start() error {
+	if server.AutoSaveInterval != 0 {
+		go server.autoSave()
+	}
+	err := redcon.ListenAndServe(fmt.Sprintf(":%d", DefaultServerPort),
+		func(conn redcon.Conn, cmd redcon.Command) {
+			switch strings.ToUpper(string(cmd.Args[0])) {
+			case "GET":
+				if len(cmd.Args) != 2 {
+					conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
+					return
+				}
+				val, ok := server.Cache.Get(string(cmd.Args[1]))
+				if !ok {
+					conn.WriteNull()
+				} else {
+					conn.WriteAny(val)
+				}
+			case "SET":
+				if len(cmd.Args) != 3 {
+					conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
+					return
+				}
+				server.Cache.Set(string(cmd.Args[1]), cmd.Args[2])
+				conn.WriteString("OK")
+			case "DEL":
+				if len(cmd.Args) != 2 {
+					conn.WriteError("ERR wrong number of arguments for '" + string(cmd.Args[0]) + "' command")
+					return
+				}
+				server.Cache.Delete(string(cmd.Args[1]))
+				conn.WriteInt(1)
+			case "PING":
+				conn.WriteString("PONG")
+			case "QUIT":
+				conn.WriteString("OK")
+				conn.Close()
+			default:
+				conn.WriteError("ERR unknown command '" + string(cmd.Args[0]) + "'")
+			}
+		},
+		func(conn redcon.Conn) bool {
+			// use this function to accept or deny the connection.
+			// log.Printf("accept: %s", conn.RemoteAddr())
+			return true
+		},
+		func(conn redcon.Conn, err error) {
+			// this is called when the connection has been closed
+			// log.Printf("closed: %s, err: %v", conn.RemoteAddr(), err)
+		},
+	)
+	return err
+}
+
+func (server *Server) autoSave() {
+	for {
+		time.Sleep(server.AutoSaveInterval)
+		err := server.Cache.SaveToFile(server.AutoSaveFile)
+		if err != nil {
+			log.Printf("error while autosaving: %s", err.Error())
+			continue
+		}
+	}
+}
