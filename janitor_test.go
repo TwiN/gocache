3554e8ea6b6b3f0d4ad9a3113d73de1bd29fd4ff
TwinProduction <twin@twinnation.org>
2020-11-02 23:45:46 -0500

    Work on #5: Improve test coverage for cache janitor

--- janitor_test.go
+++ janitor_test.go
@@ -1,6 +1,7 @@
 package gocache
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -31,4 +32,58 @@ func TestCache_StartJanitorWhenAlreadyStarted(t *testing.T) {
 		t.Fatal("expected StartJanitor to return an error, because the janitor is already started")
 	}
 	cache.StopJanitor()
+}
+
+func TestJanitor(t *testing.T) {
+	cache := NewCache().WithMaxSize(3 * JanitorMaxIterationsPerShift)
+	defer cache.Clear()
+	for i := 0; i < 3*JanitorMaxIterationsPerShift; i++ {
+		if i < JanitorMaxIterationsPerShift && i%2 == 0 {
+			cache.SetWithTTL(fmt.Sprintf("%d", i), "value", time.Millisecond)
+		} else {
+			cache.SetWithTTL(fmt.Sprintf("%d", i), "value", time.Hour)
+		}
+	}
+	cacheSize := cache.Count()
+	err := cache.StartJanitor()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cache.StopJanitor()
+	time.Sleep(JanitorMinShiftBackOff * 4)
+	if cacheSize <= cache.Count() {
+		t.Error("The janitor should be deleting expired cache entries")
+	}
+	cacheSize = cache.Count()
+	time.Sleep(JanitorMinShiftBackOff * 4)
+	if cacheSize <= cache.Count() {
+		t.Error("The janitor should be deleting expired cache entries")
+	}
+	cacheSize = cache.Count()
+	time.Sleep(JanitorMinShiftBackOff * 4)
+	if cacheSize <= cache.Count() {
+		t.Error("The janitor should be deleting expired cache entries")
+	}
+}
+
+func TestJanitorIsLoopingProperly(t *testing.T) {
+	cache := NewCache().WithMaxSize(3 * JanitorMaxIterationsPerShift)
+	defer cache.Clear()
+	cache.SetWithTTL("1", "value", time.Hour)
+	cache.SetWithTTL("2", "value", JanitorMinShiftBackOff*3)
+	cache.SetWithTTL("3", "value", JanitorMinShiftBackOff*3)
+	cache.SetWithTTL("4", "value", JanitorMinShiftBackOff*3)
+	cache.SetWithTTL("5", "value", time.Hour)
+	err := cache.StartJanitor()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer cache.StopJanitor()
+	if cache.Count() != 5 {
+		t.Error("The janitor shouldn't have had enough time to remove anything from the cache yet")
+	}
+	time.Sleep(JanitorMinShiftBackOff * 4)
+	if cache.Count() != 2 {
+		t.Error("The janitor should've deleted 3 of the 5 entries")
+	}
 }
